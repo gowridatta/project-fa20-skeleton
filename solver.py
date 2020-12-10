d266d8b4514ed30f1f66520a3f394b0784a33cc5
Gowri Datta <gowri.datta@berkeley.edu>
2020-12-09 17:09:35 -0800

    ree

--- solver.py
+++ solver.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from parse import read_input_file, write_output_file
+from parse import read_input_file, write_output_file, read_output_file
 from utils import is_valid_solution, calculate_happiness, calculate_stress_for_room, calculate_happiness_for_room
 import sys
 
@@ -21,38 +21,24 @@ def solve(G, s):
     for i in range(G.number_of_nodes()):
         D[i] = i
         rooms[i] = [i]
-        # print(G[i], "\n \n \n")
-        # print (calculate_stress_for_room(rooms[i], G))
-    # print(rooms)
     changed = []
     for i in range(G.number_of_nodes()-2):
-        
-        # print('hello')
         for j in range(G.number_of_nodes()):
             if j in list(rooms):
                 max_happy = 0
                 min_stress = s / (len(rooms) - 1)
                 curr_min_stress = min_stress
                 reee = 0
-                # print("??", list(G[j]))
                 for asdf in list(G[j]):
-                    # print(calculate_stress_for_room(rooms[D[j]] + [asdf], G) < min_stress)
-                    # print(calculate_stress_for_room(rooms[D[j]] + [asdf], G) < curr_min_stress)
-                    # print(calculate_happiness_for_room(rooms[D[j]] + [asdf], G) > max_happy)
                     if calculate_stress_for_room(rooms[D[j]] + [asdf], G) < min_stress and (calculate_stress_for_room(rooms[D[j]] + [asdf], G) < curr_min_stress or calculate_happiness_for_room(rooms[D[j]] + [asdf], G) > max_happy):
                         max_happy = calculate_happiness_for_room(rooms[D[j]] + [asdf], G)
                         curr_min_stress = calculate_stress_for_room(rooms[D[j]] + [asdf], G)
                         reee = asdf
-                    # print(asdf)
-                # print(reee, reee not in changed)
                 if reee and reee not in changed:
                     changed.append(reee)
                     D[reee] = j
-                    # print(reee, j, changed)
-                    # print(rooms,  "\n \n")
                     rooms[j].append(reee)
                     del rooms[reee]
-    print(rooms)
     return D, len(rooms)
 
 
@@ -72,6 +58,7 @@ if __name__ == '__main__':
     D, k = solve(G, s)
     assert is_valid_solution(D, G, s, k)
     print("Total Happiness: {}".format(calculate_happiness(D, G)))
+    # print("???: {}".format(calculate_happiness(read_output_file(sys.argv[2], G, s), G)))
     # write_output_file(D, 'out/test.out')
 
 
